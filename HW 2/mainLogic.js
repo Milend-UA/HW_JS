7dff3d41b3c92f2f7228bbc38d82ff5a6e0de481
Milend-UA <vitaliymilend@gmail.com>
2024-08-08 22:44:51 +0300

    HW 2 1

--- HW 2/mainLogic.js
+++ HW 2/mainLogic.js
@@ -123,7 +123,34 @@ if (abc === null || abc === "") {
 // coursesAndDurationArray. За допомоги іф перевірити кожен його елемент на
 // тривалість навчання. У випадку якщо тривалість довша за 5 місяців
 // вивести в консоль "Супер".
-
+// Вариант 1
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// if (coursesAndDurationArray[0]. monthDuration > 5) {
+//     console.log("Супер");
+// }
+// if(coursesAndDurationArray[1]. monthDuration > 5) {
+//     console.log("Супер");
+// }
+// if(coursesAndDurationArray[2]. monthDuration > 5) {
+//     console.log("Супер");
+// }
+// if(coursesAndDurationArray[3]. monthDuration > 5) {
+//     console.log("Супер");
+// }
+// if(coursesAndDurationArray[4]. monthDuration > 5) {
+//     console.log("Супер");
+// }
+// if(coursesAndDurationArray[5]. monthDuration > 5) {
+//     console.log("Супер");
+// }
+// Вариант 2
 let coursesAndDurationArray = [
     {title: 'JavaScript Complex', monthDuration: 5},
     {title: 'Java Complex', monthDuration: 6},
@@ -132,21 +159,9 @@ let coursesAndDurationArray = [
     {title: 'FullStack', monthDuration: 7},
     {title: 'Frontend', monthDuration: 4}
 ];
-if (coursesAndDurationArray[0]. monthDuration > 5) {
-    console.log("Супер");
-}
-if(coursesAndDurationArray[1]. monthDuration > 5) {
-    console.log("Супер");
-}
-if(coursesAndDurationArray[2]. monthDuration > 5) {
-    console.log("Супер");
-}
-if(coursesAndDurationArray[3]. monthDuration > 5) {
-    console.log("Супер");
-}
-if(coursesAndDurationArray[4]. monthDuration > 5) {
-    console.log("Супер");
-}
-if(coursesAndDurationArray[5]. monthDuration > 5) {
-    console.log("Супер");
-}
+console.log(coursesAndDurationArray[0].monthDuration > 5 ? "Супер" : "");
+console.log(coursesAndDurationArray[1].monthDuration > 5 ? "Супер" : "");
+console.log(coursesAndDurationArray[2].monthDuration > 5 ? "Супер" : "");
+console.log(coursesAndDurationArray[3].monthDuration > 5 ? "Супер" : "");
+console.log(coursesAndDurationArray[4].monthDuration > 5 ? "Супер" : "");
+console.log(coursesAndDurationArray[5].monthDuration > 5 ? "Супер" : "");
