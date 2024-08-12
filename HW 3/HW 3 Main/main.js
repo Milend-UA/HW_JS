12a99077ee756d8d6d57e3287c6e0538d95603f9
Milend-UA <vitaliymilend@gmail.com>
2024-08-12 13:59:46 +0300

    HW 4

--- HW 3/HW 3 Main/main.js
+++ HW 3/HW 3 Main/main.js
@@ -5,7 +5,7 @@ for (let i = 1; i <= 10; i++) {
 // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
 for (let i = 1; i <= 10; i++) {
     // document.write('<div>Lorem ipsum dolor sit amet ' + i + '</div>');
-    document.write(<div>Lorem ipsum dolor sit amet ${i}</div>);
+    document.write(`<div>Lorem ipsum dolor sit amet ${i}</div>`);
 }
 // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
 let i = 1;
