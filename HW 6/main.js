5b2ea071d36b268c29d93f0652c41e39745c4623
Milend-UA <vitaliymilend@gmail.com>
2024-08-22 17:49:11 +0300

    HW 6

--- HW 6/main.js
+++ HW 6/main.js
@@ -7,6 +7,7 @@ let str3 = 'javascript is cool';
 function characterCounter(string){
     return string.length;
 }
+// OR let characterCounter = string => string.length;
 console.log(characterCounter(str1));
 console.log(characterCounter(str2));
 console.log(characterCounter(str3));
@@ -56,7 +57,7 @@ let arr = stringToArray(str);
 console.log(arr);
 // OR
 let str = 'Ревуть воли як ясла повні';
-const stringToArray = str => str.split(' ');
+let stringToArray = str => str.split(' ');
 let arr = stringToArray(str);
 console.log(arr);
 
@@ -68,5 +69,49 @@ function convertToString(arrayElement) {
 }
 let arrayOfStrings = arrayOfNumbers.map(convertToString);
 console.log(arrayOfStrings);
+// OR
+let arrayOfNumbers = [10,8,-7,55,987,-1011,0,1050,0];
+let arrayOfStrings =
+    arrayOfNumbers.map(arrayElement => arrayElement.toString());
+console.log(arrayOfStrings);
 
+//створити функцію sortNums(array,direction), яка прймає масив чисел,
+// та сортує його від більшого до меньшого,
+// або навпаки в залежності від значення аргументу direction.
+let sortNums = (array, direction) => {
+    if (direction === 'ascending') {
+        array.sort((a, b) => a - b);
+    } else if (direction === 'descending') {
+        array.sort((a, b) => b - a);
+    }
+    return array;
+};
+let nums = [11, 21, 3];
+console.log(sortNums(nums, 'ascending'));  // [3, 11, 21]
+console.log(sortNums(nums, 'descending')); // [21, 11, 3]
 
+//- є масив
+//  -- відсортувати його за спаданням за monthDuration
+//  -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+//  -- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+coursesAndDurationArray.sort((a, b) => b.monthDuration - a.monthDuration);
+console.log(coursesAndDurationArray);
+
+let filteredArray = coursesAndDurationArray.filter(a => a.monthDuration > 5);
+console.log(filteredArray);
+
+let transformedArray = coursesAndDurationArray.map((course, index) => ({
+    id: index + 1,
+    title: course.title,
+    monthDuration: course.monthDuration
+}));
+console.log(transformedArray);
